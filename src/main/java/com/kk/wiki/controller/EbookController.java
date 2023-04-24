36043c305985416b2f000510b5d2304c8a791d6d
Cooper <radeonk@foxmail.com>
2023-04-24 14:46:21 +0800

    使用统一封装的返回对象

---
+++ src/main/java/com/kk/wiki/controller/EbookController.java
@@ -0,0 +1,25 @@
+package com.kk.wiki.controller;
+
+import com.kk.wiki.domain.Ebook;
+import com.kk.wiki.resp.CommonResp;
+import com.kk.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+    @Resource
+    private EbookService ebookService;
+    @GetMapping("/list")
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
+    }
+}
