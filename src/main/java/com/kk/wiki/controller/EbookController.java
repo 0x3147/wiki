f916ce0ae0d22dba51e7d0fbff541ac9bf9ec621
Cooper <radeonk@foxmail.com>
2023-04-24 15:10:56 +0800

    封装请求参数与返回参数

--- src/main/java/com/kk/wiki/controller/EbookController.java
+++ src/main/java/com/kk/wiki/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.kk.wiki.controller;
 
 import com.kk.wiki.domain.Ebook;
+import com.kk.wiki.req.EbookReq;
 import com.kk.wiki.resp.CommonResp;
+import com.kk.wiki.resp.EbookResp;
 import com.kk.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,9 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
     @GetMapping("/list")
-    public CommonResp<List<Ebook>> list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp<List<EbookResp>> list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
