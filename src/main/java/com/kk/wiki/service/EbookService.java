2d11e49b3d25b92aaf51c06ed57f1e0084adc86d
Cooper <radeonk@foxmail.com>
2023-04-24 15:18:49 +0800

    制作copy工具类

--- src/main/java/com/kk/wiki/service/EbookService.java
+++ src/main/java/com/kk/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.kk.wiki.domain.EbookExample;
 import com.kk.wiki.mapper.EbookMapper;
 import com.kk.wiki.req.EbookReq;
 import com.kk.wiki.resp.EbookResp;
+import com.kk.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -22,12 +23,6 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
